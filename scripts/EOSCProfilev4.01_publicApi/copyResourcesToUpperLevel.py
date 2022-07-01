bd489efc1c36d42758872dfb4e1f6a9d1eb9c056
mzouros <michael.zouros@outlook.com>
2022-07-01 14:13:55 +0300

    Refactored script to copy Providers/Resources to upper (public) level

--- scripts/EOSCProfilev4.01_publicApi/copyResourcesToUpperLevel.py
+++ scripts/EOSCProfilev4.01_publicApi/copyResourcesToUpperLevel.py
@@ -1,12 +1,10 @@
 ######################################################## IMPORTS #######################################################
 import json
 import xml.etree.ElementTree as ET
-from xml.etree.ElementTree import tostring
 import os
 from bs4 import BeautifulSoup as bs
 import argparse
 import uuid
-from xml.dom import minidom
 ######################################################## IMPORTS #######################################################
 
 
@@ -17,33 +15,26 @@ global directory
 
 ##################################################### FUNCTIONS ########################################################
 def folder_selection(directory):
-    # migrationFolders = ['/provider/', '/pending_provider/', '/infra_service/', '/pending_service/']
-    migrationFolders = ['/provider/']
+    migrationFolders = ['/provider/', '/infra_service/']
     for migrationFolder in migrationFolders:
         for file in os.listdir(directory + migrationFolder):
             if file.endswith('.json'):
-                isVersion = False
                 with open(directory + migrationFolder + file, 'r') as json_file:
-                    json_data = migrate(json_file, isVersion)
+                    migrated_data = migrate(json_file)
+                    json_data = update_core_id(migrated_data)
+                    fileName = json_data.get("id")
                     # write to file
-                    with open(directory + migrationFolder + file, 'w') as json_file:
+                    with open(directory + migrationFolder + fileName + '.json', 'w') as json_file:
                         json.dump(json_data, json_file, indent=2)
-            if file.endswith('-version'):
-                isVersion = True
-                versionFiles = os.listdir(directory + migrationFolder + file)
-                for versionFile in versionFiles:
-                    with open(directory + migrationFolder + file + '/' + versionFile, 'r') as json_file:
-                        json_data = migrate(json_file, isVersion)
-                        # write to file
-                        with open(directory + migrationFolder + file + '/' + versionFile, 'w') as json_file:
-                            json.dump(json_data, json_file, indent=2)
 
-def migrate(json_file, isVersion):
+
+def migrate(json_file):
     json_data = json.load(json_file)
     xml = json_data['payload']
 
     ET.register_namespace("tns", "http://einfracentral.eu")
     root = ET.ElementTree(ET.fromstring(xml))
+    tree = root.getroot()
 
     # copy to upper level
     service = root.find('{http://einfracentral.eu}service')
@@ -52,37 +43,39 @@ def migrate(json_file, isVersion):
         isService = True
     active = root.find('{http://einfracentral.eu}active')
     status = root.find('{http://einfracentral.eu}status')
+    metadata = root.find('{http://einfracentral.eu}metadata')
+    published = metadata.find('{http://einfracentral.eu}published')
+    identifier = root.find('{http://einfracentral.eu}identifier')
     if isService:
         latest = root.find('{http://einfracentral.eu}latest')
-    if not isVersion:
-        if active is not None and active.text == 'true':
-            if status is not None and status.text == 'approved provider':
-                provider = root.find('{http://einfracentral.eu}provider')
-                id = provider.find('{http://einfracentral.eu}id')
-                catalogueId = provider.find('{http://einfracentral.eu}catalogueId')
-                metadata = root.find('{http://einfracentral.eu}metadata')
-                published = metadata.find('{http://einfracentral.eu}metadata')
-                # identifier = root.find('{http://einfracentral.eu}identifier')
-                # if identifier is not None:
-                #     originalId = identifier.find('{http://einfracentral.eu}originalId')
-                # else:
-                #     newIdentifier = ET.Element("tns:identifier")
-                #     originalId = ET.Element("tns:originalId")
-                #     originalId.text = id.text
-                #     newIdentifier.append(originalId)
-                # if originalId is not None:
-                #     originalId.text = id.text
-                # else:
-                #     newOriginalId = ET.Element("tns:originalId")
-                #     newOriginalId.text = id.text
-                #     identifier.append(newOriginalId)
-                if id is not None and catalogueId is not None:
-                    id.text = catalogueId+'.'+id
-                if published is not None:
-                    published.text = 'true'
-                json_data = copyToUpperLevel(json_data)
-            elif isService and status.text == 'approved resource' and latest is not None and latest.text == 'true':
-                json_data = copyToUpperLevel(json_data)
+        service = root.find('{http://einfracentral.eu}service')
+        id = service.find('{http://einfracentral.eu}id')
+        catalogueId = service.find('{http://einfracentral.eu}catalogueId')
+    else:
+        provider = root.find('{http://einfracentral.eu}provider')
+        id = provider.find('{http://einfracentral.eu}id')
+        catalogueId = provider.find('{http://einfracentral.eu}catalogueId')
+    if active is not None and active.text == 'true':
+        if (status is not None and status.text == 'approved provider') or \
+                (status is not None and status.text == 'approved resource' and latest is not None and latest.text == 'true'):
+            if identifier is not None:
+                originalId = identifier.find('{http://einfracentral.eu}originalId')
+                if originalId is not None:
+                    originalId.text = id.text
+            else:
+                newIdentifier = ET.Element("tns:identifier")
+                tree.append(newIdentifier)
+                originalId = ET.Element("tns:originalId")
+                originalId.text = id.text
+                newIdentifier.append(originalId)
+            if id is not None and catalogueId is not None:
+                id.text = catalogueId.text+'.'+id.text
+            if published is not None:
+                published.text = 'true'
+            else:
+                published = ET.Element("tns:published")
+                published.text = 'true'
+                metadata.append(published)
 
     root.write('output.xml')
     with open("output.xml", "r") as xml_file:
@@ -90,17 +83,15 @@ def migrate(json_file, isVersion):
         content = "".join(content)
         bs_content = bs(content, "xml")
         json_data['payload'] = str(bs_content)
-        if isVersion:
-            json_data['resource']['payload'] = json_data['payload']
 
     return json_data
 
-def copyToUpperLevel(json_data):
+
+def update_core_id(json_data):
     newId = str(uuid.uuid4())
     updateDict = {"id": newId}
     json_data.update(updateDict)
     return json_data
-
 ##################################################### FUNCTIONS ########################################################
 
 
