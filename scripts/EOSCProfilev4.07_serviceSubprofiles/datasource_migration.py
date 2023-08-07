dc5dab1149803b15c9a456dba7253214c1835480
mzouros <michael.zouros@outlook.com>
2023-08-07 16:19:14 +0300

    Updated datasource_migration.py

--- scripts/EOSCProfilev4.07_serviceSubprofiles/datasource_migration.py
+++ scripts/EOSCProfilev4.07_serviceSubprofiles/datasource_migration.py
@@ -4,6 +4,9 @@
 # DatasourceBundle -> added "datasourceStatus", removed "status", "resourceExtras"
 
 # Strategy -> each Datasource json file to be split into 2 different json files (1 Service and 1 Datasource)
+
+      # NO NEED TO KEEP VERSION FOLDERS, MAKES IT MORE COMPLICATED WITH FILE/FOLDER RENAMES AND PARENT IDS #
+
 ######################################################## Changes #######################################################
 
 ######################################################## IMPORTS #######################################################
@@ -19,6 +22,7 @@ from unidecode import unidecode  # removes accents
 
 ##################################################### FUNCTIONS ########################################################
 def folder_selection(directory):
+    global isVersion
 
     # existing Datasoure folder. Its contents will be updated according to the new Datasource object
     datasourceFolder = '/datasource/'
@@ -31,7 +35,8 @@ def folder_selection(directory):
         if file.endswith('.json'):
             isVersion = False
             with open(directory + datasourceFolder + file, 'r') as json_file:
-                json_data = migrate_to_datasource(json_file, isVersion)
+                data = migrate_to_datasource(json_file, isVersion)
+                json_data = data[0]
                 # write to file
                 with open(directory + datasourceFolder + file, 'w') as json_file:
                     json.dump(json_data, json_file, indent=2)
@@ -40,10 +45,17 @@ def folder_selection(directory):
             versionFiles = os.listdir(directory + datasourceFolder + file)
             for versionFile in versionFiles:
                 with open(directory + datasourceFolder + file + '/' + versionFile, 'r') as json_file:
-                    json_data = migrate_to_datasource(json_file, isVersion)
+                    data = migrate_to_datasource(json_file, isVersion)
+                    json_data = data[0]
                     # write to file
                     with open(directory + datasourceFolder + file + '/' + versionFile, 'w') as json_file:
                         json.dump(json_data, json_file, indent=2)
+                # JSON files rename
+                os.rename(directory + datasourceFolder + file + '/' + versionFile,
+                          directory + datasourceFolder + file + '/' + data[1])
+        if not isVersion:
+            # print("Rename Version Folder for " + file + " to " + data[1])
+            os.rename(directory + datasourceFolder + file, directory + datasourceFolder + data[1])
 
     # Migrate Services
     for file in os.listdir(directory + serviceFolder):
@@ -71,26 +83,28 @@ def migrate_to_datasource(json_file, isVersion):
     root = ET.ElementTree(ET.fromstring(xml))
     tree = root.getroot()
 
-    status = root.find('{http://einfracentral.eu}status')
     datasource = root.find('{http://einfracentral.eu}datasource')
 
     # migrate status to datasourceStatus
-    datasourceStatus = ET.Element("tns:datasourceStatus")
+    status = root.find('{http://einfracentral.eu}status')
     if status is not None:
-        match status:
+        match status.text:
             case "approved resource":
-                datasourceStatus.text = "approved datasource"
+                status.text = "approved datasource"
             case "rejected resource":
-                datasourceStatus.text = "rejected datasource"
+                status.text = "rejected datasource"
             case "pending resource":
-                datasourceStatus.text = "pending datasource"
-        tree.append(datasourceStatus)
-        tree.remove(status)
+                status.text = "pending datasource"
 
     # remove resourceExtras field
-    resourceExtras = ET.Element("tns:resourceExtras")
+    resourceExtras = root.find('{http://einfracentral.eu}resourceExtras')
     if resourceExtras is not None:
         tree.remove(resourceExtras)
+
+    # remove identifiers field
+    identifiers = root.find('{http://einfracentral.eu}identifiers')
+    if identifiers is not None:
+        tree.remove(identifiers)
 
     # create Datasource ID
     id = datasource.find('{http://einfracentral.eu}id')
@@ -170,13 +184,14 @@ def migrate_to_datasource(json_file, isVersion):
     remove_service_related_fields(datasource, fields)
 
     # change core ID
-    coreId = create_new_id()
-    json_data['id'] = coreId
+    newCoreId = create_new_id()
+    oldCoreId = json_data['id']
+    json_data['id'] = newCoreId
     if isVersion:
-        json_data["resource"]["id"] = coreId
-        print("After the execution .json file must change from", json_data['id'], "to", coreId)
-    else:
-        print("After the execution .json file and -version folder names must change from", json_data['id'], "to", coreId)
+        json_data["resource"]["id"] = newCoreId
+        # print("After the execution .json file and -version folder names must change from", oldCoreId, "to", newCoreId)
+    # else:
+        # print("After the execution .json file must change from", oldCoreId, "to", newCoreId)
 
     root.write('output.xml')
     with open("output.xml", "r") as xml_file:
@@ -187,7 +202,7 @@ def migrate_to_datasource(json_file, isVersion):
         if isVersion:
             json_data['resource']['payload'] = json_data['payload']
 
-    return json_data
+    return json_data, newCoreId+'.json'
 
 def migrate_to_service(json_file, isVersion):
     json_data = json.load(json_file)
@@ -198,12 +213,47 @@ def migrate_to_service(json_file, isVersion):
     datasource = root.find('{http://einfracentral.eu}datasource')
 
     # delete Datasource related fields
+    submissionPolicyURL = datasource.find('{http://einfracentral.eu}submissionPolicyURL')
+    preservationPolicyURL = datasource.find('{http://einfracentral.eu}preservationPolicyURL')
+    versionControl = datasource.find('{http://einfracentral.eu}versionControl')
+    persistentIdentitySystems = datasource.find('{http://einfracentral.eu}persistentIdentitySystems')
+    jurisdiction = datasource.find('{http://einfracentral.eu}jurisdiction')
+    datasourceClassification = datasource.find('{http://einfracentral.eu}datasourceClassification')
+    researchEntityTypes = datasource.find('{http://einfracentral.eu}researchEntityTypes')
+    thematic = datasource.find('{http://einfracentral.eu}thematic')
+    researchProductLicensings = datasource.find('{http://einfracentral.eu}researchProductLicensings')
+    researchProductAccessPolicies = datasource.find('{http://einfracentral.eu}researchProductAccessPolicies')
+    researchProductMetadataLicensing = datasource.find('{http://einfracentral.eu}researchProductMetadataLicensing')
+    researchProductMetadataAccessPolicies = datasource.find('{http://einfracentral.eu}researchProductMetadataAccessPolicies')
+    fields = [submissionPolicyURL, preservationPolicyURL, versionControl, persistentIdentitySystems, jurisdiction,
+              datasourceClassification, researchEntityTypes, thematic, researchProductLicensings,
+              researchProductAccessPolicies, researchProductMetadataLicensing, researchProductMetadataAccessPolicies]
+    remove_service_related_fields(datasource, fields)
+
+    # migrate serviceType to Datasource
+    resourceExtras = root.find('{http://einfracentral.eu}resourceExtras')
+    if resourceExtras is not None:
+        serviceType = resourceExtras.find('{http://einfracentral.eu}serviceType')
+        if serviceType is not None:
+            serviceType.text = 'service_type-service'
+
     # format ID
+    id = datasource.find('{http://einfracentral.eu}id')
+    if id is not None:
+        abbreviation = datasource.find('{http://einfracentral.eu}abbreviation')
+        resourceOrganisation = datasource.find('{http://einfracentral.eu}resourceOrganisation')
+        if abbreviation is not None and resourceOrganisation is not None:
+            if abbreviation.text is not None and resourceOrganisation.text is not None:
+                id.text = format_string(resourceOrganisation.text, abbreviation.text)
+
+    # change Payload's tags
+    root.getroot().tag = "{http://einfracentral.eu}serviceBundle"
+    datasource.tag = '{http://einfracentral.eu}service'
 
     # change resourceTypeName
     json_data['resourceTypeName'] = 'service'
     if isVersion:
-        json_data["resource"]["resourceTypeName"] = "service"
+        json_data["resource"]["resourceTypeName"] = 'service'
 
     root.write('output.xml')
     with open("output.xml", "r") as xml_file:
